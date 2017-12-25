6f199f7de9b0911c27531700eec85a14fe75ea71
Bapi Gupta <rajat310198@outlook.com>
2017-12-25 15:56:27 +0530

    Adding website

---
+++ js/hack.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".button-collapse").sideNav();
+    $('.parallax').parallax();
+
+});
+/*
+$(document).on('click', 'a[href^="#"]', function(e){
+    var id = $(this).attr(href);
+    var $id = $(id);
+    if($id.lenght==0){
+        return;
+    }
+
+    e.preventDefault();
+    var pos = $id.offset().top;
+    $('body, html').animate(scrollTop: pos);
+});*/
+
